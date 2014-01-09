e5279781808238489cd494d3a480aa4094849a06
vivekraghuram <vv9295@yahoo.com>
2014-01-08 19:23:09 -0800

    Finished redesign macro stuff

--- js/nav.js
+++ js/nav.js
@@ -16,26 +16,4 @@ function navigate(navid) {
 	var selectnav = $('#'+navid);
 	$(selectnav).addClass('chosen');
 
-/*
-
-	var items = $('.helppanel, .helppanelbutton, .xbox');
-	var navlink = $('.navlink');
-	var current = navlink.id;
-	var xbox = $('.xbox')
-
-	var open = function() {
-							$(items).removeClass('close').addClass('open');
-						}
-	var close = function() { 
-							$(items).removeClass('open').addClass('close');
-						}
-
-	hp.click(function(){
-		if (hp.hasClass('open')) {$(close)}
-		else {$(open)}
-	});
-	xbox.click(function(){
-		if (hp.hasClass('open')) {$(close)};
-	});*/
-
 }
